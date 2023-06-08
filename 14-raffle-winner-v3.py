4239b05a90a4650ca7038a88c2f0413fa0de0e94
Fuiiyoh <100391116+Fuiiyoh@users.noreply.github.com>
2023-06-08 12:50:19 +1200

    14-raffle-winner-v3

---
+++ 14-raffle-winner-v3.py
@@ -0,0 +1,37 @@
+import pandas
+import random
+
+# currency fortmatting function
+def currency(x):
+  return "${:.2f}".format(x)
+
+# dictionaires to hold ticket details
+all_names = ["a", "b", "c", "d", "e"]
+all_ticket_cost = [7.50, 7.50, 10.50, 10.50, 6.50]
+surcharge = [0, 0, 0.53, 0.53, 0]
+
+mini_movie_dict = {
+  "Name": all_names,
+  "Ticket Price": all_ticket_cost,
+  "Surcharge": surcharge
+}
+
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+#mini_movie_frame = mini_movie_frame.set_index('Name')
+
+# Calculate the total ticket cost (ticket + surcharge)
+mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] + mini_movie_frame['Ticket Price']
+
+# calculate profit for each ticket
+mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
+
+# choose a winner from our name list
+winner = random.choice(all_names)
+
+# get position of winner in name list
+win_index = all_names.index(winner)
+
+# look up winner in panda and output their data
+
+test = mini_movie_frame._get_value(win_index, 'Name')
+print("test", test)
